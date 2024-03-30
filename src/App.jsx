9a748f0bcf8600edad7f4a71cfa35836c0e57c79
unknown <stefanhabets92@gmail.com>
2024-03-30 13:22:27 +0100

    Added backend and added react query

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import SearchMeal from './components/search-meals';
+import FetchTopQuotes from './components/top-quotes';
+import { ToastContainer } from 'react-toastify';
 import Users from './components/users';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
+      <QueryClientProvider client={queryClient}>
+        <ToastContainer />
+        <FetchTopQuotes />
+      </QueryClientProvider>
       <SearchMeal />
       <Users />
     </>
