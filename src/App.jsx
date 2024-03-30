c2726831c1529d0ab8122f3a049a1ef08702e16e
unknown <stefanhabets92@gmail.com>
2024-03-30 14:01:01 +0100

    Added paginated quotes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { ToastContainer } from 'react-toastify';
 import Users from './components/users';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import UpdateQuotes from './components/update-quote';
+import PaginatedQuotes from './components/paginated-quotes';
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ function App() {
     <>
       <QueryClientProvider client={queryClient}>
         <ToastContainer />
+        <PaginatedQuotes />
         <UpdateQuotes />
         <FetchTopQuotes />
       </QueryClientProvider>
